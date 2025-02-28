316b3de64b99e4eb0360e377c2fe9483321d3ae3
sk-nahid <sknahid0111@gmail.com>
2025-02-28 15:39:02 +0600

    2nd commit

--- script/login.js
+++ script/login.js
@@ -1,15 +1,16 @@
-const pin = getValueById("account-password");
-const account = document.getElementById("account-number").value;
-const accountLength= account.length;
-
-document.getElementById('btn-login').addEventListener('click', function(event){
+document
+  .getElementById("btn-login")
+  .addEventListener("click", function (event) {
     event.preventDefault();
-    if(accountLength === 11){
-        if(pin === 1234){
-            window.location.href="./home.html"
-        }
+    const pin = getValueById("account-password");
+    const account = document.getElementById("account-number").value;
+    const accountLength = account.length;
+
+    if (accountLength === 11) {
+      if (pin === 1234) {
+        window.location.href = "./home.html";
+      }
+    } else {
+      alert("not done");
     }
-    else{
-        alert( 'not done')
-    }
-})
+  });
